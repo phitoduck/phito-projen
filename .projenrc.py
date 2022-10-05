e64891573de7abe27b6036c0f1f99c26eeca2131
Eric Riddoch <eric.riddoch@bengroup.com>
2022-10-05 02:37:49 -0600

    released 0.1.0

--- .projenrc.py
+++ .projenrc.py
@@ -5,7 +5,7 @@ project = PythonPackage(
     name="phitoduck-projen",
     module_name="phito_projen",
     install_requires=["projen", "jinja2"],
-    version="0.0.2",
+    version="0.1.0",
 )
 project.manifest_in.add_recursive_include("src/", "*template*", comment="include template files for rendering components")
 
